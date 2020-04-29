d4b3fd8702cef120be43a595087fe39f388e8260
Joshua Singleton <jsingle345@gmail.com>
2020-04-29 18:36:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,7 +3,7 @@ require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
-
+binding.pry
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
@@ -13,4 +13,3 @@ def engage
   puts state_log(date)
   date = generate_star_date
 end
-binding.pry
